5077af504c2636f26b5b68ced0bf858d1d6689d7
Rifqi Oktario <rifqi.oktario@dansmultipro.com>
2025-03-02 12:19:43 +0700

    feat: add reference and init materialized view

---
+++ RAW_QUERY/9-create_materialized_view_employee_report.sql
@@ -0,0 +1,32 @@
+CREATE MATERIALIZED VIEW employee_summary AS
+WITH family_counts AS (
+    SELECT
+        employee_id,
+        relation,
+        COUNT(*) AS total
+    FROM public.employee_family
+    GROUP BY employee_id, relation
+)
+SELECT
+    e.id AS employee_id,
+    e.nik,
+    e.name,
+    e.is_active,
+    ep.gender,
+    EXTRACT(YEAR FROM age(NOW(), ep.date_of_birth)) AS age,
+    ed.name AS school_name,
+    ed.level,
+    COALESCE(
+        STRING_AGG(fc.total || ' ' || fc.relation, ' & '), 
+        '-'
+    ) AS family_data
+FROM
+    public.employee e
+    INNER JOIN public.employee_profile ep ON e.id = ep.employee_id
+    INNER JOIN public.education ed ON e.id = ed.employee_id
+    LEFT JOIN family_counts fc ON e.id = fc.employee_id
+GROUP BY
+    e.id, e.nik, e.name, e.is_active, ep.gender, ep.date_of_birth, ed.name, ed.level
+WITH DATA;
+
+CREATE INDEX idx_employee_summary_id ON employee_summary(employee_id);
